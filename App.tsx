a352310c8eb7a235124848c4d4ab93a9f3aa3352
GuiQRSM <endbeob15@gmail.com>
2023-08-30 15:54:29 -0300

    Add color and size styles to fonts and resize and colorize the screen.

--- App.tsx
+++ App.tsx
@@ -2,9 +2,32 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{}}>
-      <Text>UI</Text>
-      <Text>Test</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#131016",
+        padding: 48,
+      }}
+    >
+      <Text
+        style={{
+          color: "#FFF",
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 18,
+        }}
+      >
+        Nome do evento
+      </Text>
+
+      <Text
+        style={{
+          color: "#bfbfbf",
+          fontSize: 16,
+        }}
+      >
+        Sexta, 4 de outobro de 2023
+      </Text>
     </View>
   );
 }
