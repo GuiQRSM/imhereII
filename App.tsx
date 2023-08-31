936c2e9c8ff08ded117e508280b6098c20367b8b
GuiQRSM <endbeob15@gmail.com>
2023-08-31 16:45:00 -0300

    Organize the project.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,5 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de outobro de 2023</Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 48,
-  },
-  eventName: {
-    color: "#FFF",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 18,
-  },
-  eventDate: {
-    color: "#bfbfbf",
-    fontSize: 16,
-  },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 48,
+  },
+  eventName: {
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 18,
+  },
+  eventDate: {
+    color: "#bfbfbf",
+    fontSize: 16,
+  },
+});
