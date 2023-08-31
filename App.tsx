c5f7d7d455a58c82fa5469119073ef862db7bf58
GuiQRSM <endbeob15@gmail.com>
2023-08-31 15:53:19 -0300

    Assigns two more elements to the const styles.

--- App.tsx
+++ App.tsx
@@ -2,26 +2,9 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={{}}>
-      <Text
-        style={{
-          color: "#FFF",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginTop: 18,
-        }}
-      >
-        Nome do evento
-      </Text>
-
-      <Text
-        style={{
-          color: "#bfbfbf",
-          fontSize: 16,
-        }}
-      >
-        Sexta, 4 de outobro de 2023
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de outobro de 2023</Text>
     </View>
   );
 }
@@ -31,5 +14,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#131016",
     padding: 48,
+  },
+  eventName: {
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 18,
+  },
+  eventDate: {
+    color: "#bfbfbf",
+    fontSize: 16,
   },
 });
