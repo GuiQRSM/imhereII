7c57b6e20e05354b020b9df1e0d9e6968af8ec6a
GuiQRSM <endbeob15@gmail.com>
2023-09-01 16:25:41 -0300

    Configures the TextInput to be rendered.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,6 +6,11 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de outobro de 2023</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
     </View>
   );
 }
