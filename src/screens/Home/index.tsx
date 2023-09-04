6dbbbead7bede81c25c3685d28ecd1da55c587e5
GuiQRSM <endbeob15@gmail.com>
2023-09-03 23:23:19 -0300

    fixed failure to locate folders.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
-import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
+import { styles } from "./styles";
 
 export function Home() {
   function handleParticipantAdd() {
@@ -22,6 +23,8 @@ export function Home() {
           <Text style={styles.buttonText}> + </Text>
         </TouchableOpacity>
       </View>
+
+      <Participant />
     </View>
   );
 }
