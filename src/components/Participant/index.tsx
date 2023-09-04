34d2f08caefbf18c70388e7863a1f2227199a277
GuiQRSM <endbeob15@gmail.com>
2023-09-04 16:11:03 -0300

    adicione a tipagem 'Props' no componente.

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -1,10 +1,14 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-export function Participant(props) {
+type = {
+  Props: String,
+};
+
+export function Participant({ name }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{props.name}</Text>
+      <Text style={styles.name}>{name}</Text>
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}> - </Text>
       </TouchableOpacity>
