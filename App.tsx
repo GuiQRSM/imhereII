1a040b8ebe023cfc34ff911530f8c57ef31347ca
GuiQRSM <endbeob15@gmail.com>
2023-09-11 16:08:54 -0300

    Final StatusBar style settings.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Home } from "./src/screens/Home";
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" translucent />
+      <StatusBar barStyle="light-content" />
       <Home />
     </>
   );
